1fcddccfb61a55d8f34b431ad4cd06bbe731bf08
danismag <danismag@bk.ru>
2017-02-09 15:59:39 +0300

    + отдельный контроллер Genre

---
+++ protected/Controllers/Genre.php
@@ -0,0 +1,76 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use T4\Core\MultiException;
+use T4\Mvc\Controller;
+
+class Genre
+    extends Controller
+{
+    public function actionNew()
+    {
+
+    }
+
+    public function actionEdit($id)
+    {
+        $genre = \App\Models\Genre::findByPK($id);
+
+        if (false === $genre) {
+
+            $this->app->flash->message = 'Жанр не найден';
+            $this->redirect('/genre/editAll');
+        }
+        $this->data->genre = $genre;
+    }
+
+    public function actionSave($genre = null)
+    {
+        if (null !== $genre) {
+
+            try {
+                if (empty($genre['pk'])) {
+                    $obj = new \App\Models\Genre;
+
+                } else {
+                    $obj = \App\Models\Genre::findByPK($genre['pk']);
+                }
+                $obj->fill($genre)
+                    ->save();
+                $this->app->flash->message = 'Жанр успешно сохранен';
+                $this->redirect('/genre/editAll');
+
+            } catch (MultiException $exc) {
+
+                $this->data->errors = $exc;
+                $this->data->genre = $genre;
+            }
+        }
+    }
+
+    public function actionEditAll()
+    {
+        $this->data->genres = \App\Models\Genre::findAll();
+    }
+
+    public function actionDelete($id)
+    {
+        $genre = \App\Models\Genre::findByPK($id);
+
+        if (false !== $genre) {
+
+            $genre->delete();
+            $this->app->flash->message = 'Жанр успешно удален';
+
+        } else {
+
+            $this->app->flash->message = 'Жанр не найден';
+        }
+
+        $this->redirect('/genre/editAll');
+    }
+
+}
